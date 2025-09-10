84335c59756d2daa61d5f0df84bade3c180fa440
faizikhsan123 <faizashary19@gmail.com>
2025-09-10 10:30:39 +0700

    Migrasi dan model pada Employee

---
+++ database/migrations/2025_09_10_032442_create_employees_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_lengkap', 100);
+            $table->string('email', 100)->unique();
+            $table->string('nomor_telepon', 15)->unique();
+            $table->string('alamat');
+            $table->date('tanggal_masuk');
+            $table->enum('status',['aktif','non-aktif'])->default('aktif');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
